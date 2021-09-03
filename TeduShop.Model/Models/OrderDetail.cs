69bd373bbbf66aec3666fb2c138276af98f33944
DESKTOP\phi <nguyenducphi.81@gmail.com>
2021-09-03 22:14:13 +0700

    Add domain entities for project

---
+++ TeduShop.Model/Models/OrderDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("OrderDetails")]
+    public class OrderDetail
+    {
+        [Key]
+        public int OrderID { set; get; }
+
+        [Key]
+        public int ProductID { set; get; }
+
+        public int Quantity { set; get; }
+
+        [ForeignKey("OrderID")]
+        public virtual Order Order { set; get; } // Tự động mapping quan hệ với cột ID trong bảng Orders khi render ra Database
+
+        [ForeignKey("ProductID")]
+        public virtual Product Product { set; get; } // Tự động mapping quan hệ với cột ID trong bảng Products khi render ra Database
+    }
+}
---
+++ TeduShop.Model/Models/PostTag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("PostTags")]
+    public class PostTag
+    {
+        [Key]
+        public int PostID { set; get; }
+
+        [Key]
+        [MaxLength(50)]
+        [Column(TypeName = "varchar")]
+        public string TagID { set; get; }
+
+        [ForeignKey("TagID")]
+        public virtual Tag Tag { set; get; }  // Tự động mapping quan hệ với cột ID trong bảng Tags khi render ra Database
+
+        [ForeignKey("PostID")]
+        public virtual Post Post { set; get; }   // Tự động mapping quan hệ với cột ID trong bảng Posts khi render ra Database
+    }
+}
