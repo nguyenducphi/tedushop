732e94592156bf3bef15e5d654e367cc885180ea
DESKTOP\phi <nguyenducphi.81@gmail.com>
2021-09-04 18:43:46 +0700

    Commit migration and repository

--- TeduShop.Data/Infrastructure/IRepository.cs
+++ TeduShop.Data/Infrastructure/IRepository.cs
@@ -10,23 +10,29 @@ namespace TeduShop.Data.Infrastructure
     public interface IRepository<T> where T : class
     {
         // Marks an entity as new
-        void Add(T entity);
+        T Add(T entity);
 
+        // Marks an entity as modified
         void Update(T entity);
 
-        void Delete(T entity);
+        // Marks an entity to be removed
+        T Delete(T entity);
 
+        T Delete(int id);
+
+        //Delete multi records
         void DeleteMulti(Expression<Func<T, bool>> where);
 
-        T GetSingleByID(int id);
+        // Get an entity by int id
+        T GetSingleById(int id);
 
         T GetSingleByCondition(Expression<Func<T, bool>> expression, string[] includes = null);
 
-        IQueryable<T> GetAll(string[] includes = null);
+        IEnumerable<T> GetAll(string[] includes = null);
 
-        IQueryable<T> GetMulti(Expression<Func<T, bool>> predicate, string[] includes = null);
+        IEnumerable<T> GetMulti(Expression<Func<T, bool>> predicate, string[] includes = null);
 
-        IQueryable<T> GetMultiPaging(Expression<Func<T, bool>> filter, out int total, int index = 0, int size = 50, string[] includes = null);///
+        IEnumerable<T> GetMultiPaging(Expression<Func<T, bool>> filter, out int total, int index = 0, int size = 50, string[] includes = null);
 
         int Count(Expression<Func<T, bool>> where);
 
--- TeduShop.Model/Models/PostTag.cs
+++ TeduShop.Model/Models/PostTag.cs
@@ -12,11 +12,12 @@ namespace TeduShop.Model.Models
     public class PostTag
     {
         [Key]
+        [Column(Order = 1)]
         public int PostID { set; get; }
 
         [Key]
         [MaxLength(50)]
-        [Column(TypeName = "varchar")]
+        [Column(TypeName = "varchar", Order = 2)]
         public string TagID { set; get; }
 
         [ForeignKey("TagID")]
